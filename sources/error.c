46ec37de3983bc4d543a4e8b5c14186cc82e1cc5
lorobert <lorobert@student.42lausanne.ch>
2023-05-24 15:09:14 +0200

    Better color error

--- sources/error.c
+++ sources/error.c
@@ -6,7 +6,7 @@
 /*   By: lorobert <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 11:28:12 by lorobert          #+#    #+#             */
-/*   Updated: 2023/05/24 14:33:12 by lorobert         ###   ########.fr       */
+/*   Updated: 2023/05/24 15:08:19 by lorobert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,9 @@
 
 static void	check_color(t_color color)
 {
-	if (color.red < 0 || color.red > 255)
-		fatal_error("Invalid color\n");
-	if (color.green < 0 || color.green > 255)
-		fatal_error("Invalid color\n");
-	if (color.blue < 0 || color.blue > 255)
+	if (color.red < 0 || color.green < 0 || color.blue < 0)
+		fatal_error("Missing color\n");
+	if (color.red > 255 || color.green > 255 || color.blue > 255)
 		fatal_error("Invalid color\n");
 }
 
