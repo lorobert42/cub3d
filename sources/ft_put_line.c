97a1a81c8a496a05df5c0756f97dd152c12d5569
SHiroz <shiroz@student.42lausanne.ch>
2023-05-31 15:36:03 +0200

    hehe avant toi

--- sources/ft_put_line.c
+++ sources/ft_put_line.c
@@ -6,7 +6,7 @@
 /*   By: shiroz <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 15:15:11 by shiroz            #+#    #+#             */
-/*   Updated: 2023/05/31 15:03:40 by shiroz           ###   ########.fr       */
+/*   Updated: 2023/05/31 15:35:11 by shiroz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,18 @@ void	ft_put_line(t_info *info)
 
    	i = 0;
 	info->line_to_print.wall_size = (HEIGHT - info->line_to_print.wall_size) / 2;
-	printf("ICI%d\n", info->line_to_print.wall_size);
+//	printf("ICI%d\n", info->line_to_print.wall_size);
 	while (i < (unsigned int)info->line_to_print.wall_size)
 	{
-//		tmp = (unsigned int *)(&info->image.image[(i * WIDTH + 500) * 4]);
-	//	printf("%d\n", info->line_to_print.n_col);
 		tmp = (unsigned int *)(&info->image.image[(WIDTH * i + \
 		info->line_to_print.n_col) * 4 /* info->image.bits_per_pixel*/]);
 		*tmp = info->floor;
 
-		tmp = (unsigned int *)(&info->image.image[(WIDTH * i + \
-		info->line_to_print.n_col) * 4 /* info->image.bits_per_pixel*/]);
-		*tmp = info->floor;
+
+		tmp = (unsigned int *)(&info->image.image[((WIDTH) * (HEIGHT - 1 - i) +  \
+		info->line_to_print.n_col) * 4]);
+
+		*tmp = info->ceiling;
 		i++;
 	}
 }
